11a7d0905520e3176d5c8bbfa402a0dbbef4c60b
ditternation <ditternation@outlook.com>
2026-04-16 07:33:54 +0200

    added several camera-fixes

--- platform/exynos2100/patches/miscs/customize.sh
+++ platform/exynos2100/patches/miscs/customize.sh
@@ -6,40 +6,8 @@ SET_PROP "vendor" "renderthread.skia.reduceopstasksplitting" "true"
 SET_PROP "vendor" "debug.hwui.skia_atrace_enabled" "false"
 LOG_STEP_OUT
 
-LOG_STEP_IN "- Setting FUSE passthough"
-SET_PROP "vendor" "persist.sys.fuse.passthrough.enable" "true"
-LOG_STEP_OUT
-
 LOG "- Disabling encryption"
 # Encryption
 LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.exynos2100")
 sed -i "${LINE}s/,fileencryption=aes-256-xts:aes-256-cts:v2//g" "$WORK_DIR/vendor/etc/fstab.exynos2100"
 
-# ODE
-sed -i -e "/ODE/d" -e "/keydata/d" -e "/keyrefuge/d" "$WORK_DIR/vendor/etc/fstab.exynos2100"
-
-LOG_STEP_IN "- Setting stock Bluetooth profiles"
-SET_PROP "product" "bluetooth.profile.asha.central.enabled" "true"
-SET_PROP "product" "bluetooth.profile.a2dp.source.enabled" "true"
-SET_PROP "product" "bluetooth.profile.avrcp.target.enabled" "true"
-SET_PROP "product" "bluetooth.profile.bap.broadcast.assist.enabled" "false"
-SET_PROP "product" "bluetooth.profile.bap.broadcast.source.enabled" "false"
-SET_PROP "product" "bluetooth.profile.bap.unicast.client.enabled" "false"
-SET_PROP "product" "bluetooth.profile.bas.client.enabled" "false"
-SET_PROP "product" "bluetooth.profile.csip.set_coordinator.enabled" "false"
-SET_PROP "product" "bluetooth.profile.gatt.enabled" "true"
-SET_PROP "product" "bluetooth.profile.hap.client.enabled" "false"
-SET_PROP "product" "bluetooth.profile.hfp.ag.enabled" "true"
-SET_PROP "product" "bluetooth.profile.hid.device.enabled" "true"
-SET_PROP "product" "bluetooth.profile.hid.host.enabled" "true"
-SET_PROP "product" "bluetooth.profile.map.server.enabled" "true"
-SET_PROP "product" "bluetooth.profile.mcp.server.enabled" "false"
-SET_PROP "product" "bluetooth.profile.opp.enabled" "false"
-SET_PROP "product" "bluetooth.profile.pan.nap.enabled" "true"
-SET_PROP "product" "bluetooth.profile.pan.panu.enabled" "true"
-SET_PROP "product" "bluetooth.profile.pbap.server.enabled" "true"
-SET_PROP "product" "bluetooth.profile.sap.server.enabled" "true"
-SET_PROP "product" "bluetooth.profile.ccp.server.enabled" "false"
-SET_PROP "product" "bluetooth.profile.vcp.controller.enabled" "false"
-
-LOG_STEP_OUT
--- unica/patches/stock_blobs/customize.sh
+++ unica/patches/stock_blobs/customize.sh
@@ -30,11 +30,9 @@ SET_PROP "product" "ro.gfx.driver.0" "$(GET_PROP "$WORK_DIR/vendor/build.prop" "
 SET_PROP "product" "ro.gfx.driver.1" "$(GET_PROP "$WORK_DIR/vendor/build.prop" "ro.gfx.driver.1")"
 LOG_STEP_OUT
 
-if [[ "$SOURCE_PRODUCT_FIRST_API_LEVEL" -gt 33 && "$TARGET_PRODUCT_FIRST_API_LEVEL" -le 33 ]]; then
-    LOG_STEP_IN "- Downgrading ENGMODE JNI"
-    DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.security.engmode-V1-ndk.so"
-    ADD_TO_WORK_DIR "r11sxxx" "system" "lib64/lib.engmode.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
-    ADD_TO_WORK_DIR "r11sxxx" "system" "lib64/lib.engmodejni.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
-    ADD_TO_WORK_DIR "r11sxxx" "system" "lib64/vendor.samsung.hardware.security.engmode@1.0.so" 0 0 644 "u:object_r:system_lib_file:s0"
-    LOG_STEP_OUT
-fi
+LOG_STEP_IN "- Downgrading ENGMODE JNI"
+DELETE_FROM_WORK_DIR "system" "system/lib64/vendor.samsung.hardware.security.engmode-V1-ndk.so"
+ADD_TO_WORK_DIR "r11sxxx" "system" "lib64/lib.engmode.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "r11sxxx" "system" "lib64/lib.engmodejni.samsung.so" 0 0 644 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "r11sxxx" "system" "lib64/vendor.samsung.hardware.security.engmode@1.0.so" 0 0 644 "u:object_r:system_lib_file:s0"
+LOG_STEP_OUT
